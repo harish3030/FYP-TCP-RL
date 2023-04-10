552c78289c492d3b632f2e19c4d6f462e797b2b6
harish3030 <harishjegan30@gmail.com>
2023-04-10 12:21:59 +0530

    Adding plots

--- PPO-Agent/PPO-Agent.py
+++ PPO-Agent/PPO-Agent.py
@@ -192,11 +192,6 @@ for e in range(iterationNum):
     tp_history=[]
     rtt_history=[]
     rew_history=[]
-    
-    # if the have at least batch_size experiences in the memory buffer
-    # than we tain our model
-    
-
 
 print(tp)       
 mpl.rcdefaults()
@@ -204,49 +199,4 @@ mpl.rcParams.update({'font.size': 16})
 plot_graphs(global_cWnd_history,'Congestion Windows','CWND (segments)',iterationNum)
 plot_graphs(global_tp_history,'Throughput over time','Throughput (Mbits/s)',iterationNum)
 plot_graphs(global_rtt_history,'RTT over time','RTT (seconds)',iterationNum)
-plot_graphs(global_rew_history,'Reward sum plot','Accumulated reward',iterationNum)  
-    
-    # N = 20
-    # batch_size = 5
-    # n_epochs = 4
-    # alpha = 0.0003
-    # agent = Agent(n_actions=3, batch_size=batch_size,
-    #               alpha=alpha, n_epochs=n_epochs,
-    #               input_dims=env.observation_space.shape)
-    # n_games = 300
-
-    # best_score = env.reward_range[0]
-    # score_history = []
-
-    # learn_iters = 0
-    # avg_score = 0
-    # n_steps = 0
-
-    # for i in range(n_games):
-    #     observation = env.reset()
-    #     done = False
-    #     score = 0
-    #     while not done:
-    #         action, prob, val = agent.choose_action(np.expand_dims(observation, axis=0))
-    #         observation_, reward, done, info = env.step(action)
-    #         n_steps += 1
-    #         score += reward
-    #         agent.store_transition(observation, action,
-    #                                prob, val, reward, done)
-    #         if n_steps % N == 0:
-    #             agent.learn()
-    #             learn_iters += 1
-    #         observation = observation_
-    #     score_history.append(score)
-    #     avg_score = np.mean(score_history[-100:])
-        
-    #     if avg_score > best_score:
-    #         best_score = avg_score
-    #         agent.save_models()
-    #     tf.summary.scalar('reward summary', data=avg_score, step=i)
-    #     print('episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score,
-    #           'time_steps', n_steps, 'learning_steps', learn_iters)
-    #     env.render()
-    # filename = 'PPO_trading_view.png'
-    # x = [i+1 for i in range(len(score_history))]
-    # plot_learning_curve(x, score_history, figure_file)
+plot_graphs(global_rew_history,'Reward sum plot','Accumulated reward',iterationNum) 
