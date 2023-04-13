df1160c6817aace144c0713c4c39b4e3c21409ba
Harish J <66659601+harish3030@users.noreply.github.com>
2023-04-13 09:33:01 +0530

    Update DQN-Agent.py

--- DQN-Agent/DQN-Agent.py
+++ DQN-Agent/DQN-Agent.py
@@ -19,6 +19,7 @@ from tensorflow.keras.metrics import mean_squared_error
 from ns3gym import ns3env
 from tcp_base import TcpTimeBased, TcpEventBased
 
+
 try:
     w_file = open('run.log', 'w')
 except:
@@ -36,11 +37,21 @@ parser.add_argument('--steps',
                     type=int,
                     default=1000,
                     help='Number of steps, Default 100')
+parser.add_argument('--bandwidth',
+                    type=int,
+                    default=1e7,
+                    help='Number of steps, Default 100')
+parser.add_argument('--leafs',
+                    type=int,
+                    default=5,
+                    help='Number of steps, Default 100')
 args = parser.parse_args()
 
 startSim = bool(args.start)
 iterationNum = int(args.iterations)
 maxSteps = int(args.steps)
+bandwidth=int(args.bandwidth)
+leafs=int(args.leafs)
 print(maxSteps)
 port = 5556
 simTime = 10000 # seconds
@@ -68,16 +79,17 @@ class DQNAgent:
         self.lr = 0.001
         self.gamma = 0.99
         self.exploration_proba = 1.0
-        self.exploration_proba_decay = ((2*iterationNum*maxSteps)-1)/(2*iterationNum*maxSteps)
+        self.exploration_proba_decay = ((iterationNum*maxSteps)-1)/(iterationNum*maxSteps)
+        # self.exploration_proba_decay = 0.1
         #self.exploration_proba_decay = 1/(iterationNum*maxSteps)
-        #self.exploration_proba_decay = 0.005
+        # self.exploration_proba_decay = 0.005
         self.min_epsilon=0.1
-        self.batch_size = 32
+        self.batch_size = 3
         
         # We define our memory buffer where we will store our experiences
         # We stores only the 2000 last time steps
         self.memory_buffer= list()
-        self.max_memory_buffer = 1200
+        self.max_memory_buffer = 1000
         
         # We creaate our model having to hidden layers of 24 units (neurones)
         # The first layer has the same size as a state size
@@ -93,7 +105,7 @@ class DQNAgent:
                       metrics=['accuracy'])
         
     # The agent computes the action to perform given a state 
-    def compute_action(self, current_state):
+    def compute_action(self, current_state,node_id):
         # We sample a variable uniformly over [0,1]
         # if the variable is less than the exploration probability
         #     we choose an action randomly
@@ -107,10 +119,17 @@ class DQNAgent:
             return action_index
         q_values = self.model.predict(current_state)[0]
         action_index= np.argmax(q_values)
+        
         print("\t[*] Exploiting gained knowledge. Selected action: {}".format(action_index), 
               file=w_file)
         return action_index
     
+    def predict_action(self,current_state):
+        q_values = self.model.predict(current_state)[0]
+        action_index= np.argmax(q_values)
+        print("\t[*] Exploiting gained knowledge. Selected action: {}".format(action_index), 
+              file=w_file)
+        return action_index
     #Save Model
     def save_model(self):
         self.model.save("DQN_model")   
@@ -128,8 +147,9 @@ class DQNAgent:
     # we update the exploration probability using 
     # espilon greedy algorithm
     def update_exploration_probability(self):
-         if self.exploration_proba > self.min_epsilon:
+          if self.exploration_proba > self.min_epsilon:
             self.exploration_proba*=self.exploration_proba_decay
+        # self.exploration_proba = self.exploration_proba * np.exp(-self.exploration_proba_decay)
         
     def find_exploration_proba(self):
         return self.exploration_proba
@@ -149,10 +169,10 @@ class DQNAgent:
     
 
     # At the end of each episode, we train our model
-    def train(self):
+    def train(self,node_id):
         # We shuffle the memory buffer and select a batch size of experiences
         #np.random.shuffle(self.memory_buffer)
-        batch_sample = self.memory_buffer[0:self.batch_size]
+        batch_sample = self.memory_buffer[-1:-1-self.batch_size:-1]
         
         # We iterate over the selected experiences
         for experience in batch_sample:
@@ -163,6 +183,7 @@ class DQNAgent:
             if not experience["done"]:
                 q_target = q_target + self.gamma*np.max(self.model.predict(
                                                      experience["next_state"])[0])
+              
             q_current_state[0][experience["action"]] = q_target
             # train the model
             self.model.fit(experience["current_state"], q_current_state, verbose=0)
@@ -178,18 +199,19 @@ action_mapping[2]=-150
 # action_mapping[0]=0
 # action_mapping[1]=1
 # action_mapping[2]=-1
-# action_mapping[3]=2
-# action_mapping[4]=3
+# action_mapping[3]=3
+
 
 agent = DQNAgent(state_size, action_size)
+#agent.load_model()
 total_steps = 0
 rewardsum=0
-
-rew_history = []
-cWnd_history = []
-pred_cWnd_history = []
-rtt_history = []
-tp_history = []
+print("LEafs: ",leafs)
+rew_history = [[] for i in range(leafs)]
+cWnd_history = [[] for i in range(leafs)]
+rtt_history = [[] for i in range(leafs)]
+tp_history = [[] for i in range(leafs)]
+print("Size:",len(rew_history))
 tp=[]
 global_cWnd_history=[]
 global_rtt_history=[]
@@ -214,34 +236,90 @@ def mkdir_p(mypath):
         if exc.errno == EEXIST and path.isdir(mypath):
             pass
         else: raise
-def plot_graphs(history,tit,label,episode):
-
-    plt.plot(range(len(history)),history,marker="",linestyle="-")
+col=["red","green","blue","pink","grey","brown"]
+def plot_individual_graphs(history,tit,label,episode):
+   
+    
     plt.title(tit)
     plt.xlabel('Steps')
+    plt.ylabel(label)
+    leg=[]
+    # for i in range(1,leafs):
+    #     while len(history[i])!=len(history[0]):
+    #         history[i].append(history[i][-1])
+    for i in range(leafs):
+       plt.plot(range(len(history[i])),history[i],marker="",linestyle="-",color=col[i])
+       leg.append(str(i+1))
+    plt.legend(leg)   
+    
+    output_dir = f"./Plots/Episode-{episode}"
+    mkdir_p(output_dir)
+    file_name=f'{tit}.png'
+    plt.savefig(f"{output_dir}/{file_name}",bbox_inches='tight')
+    plt.show()
+def plot_graphs(history,tit,label,episode):
+    #print(history)
+    episode=episode+1
+    
+    plt.plot(range(len(history)),history,marker="",linestyle="-",color="red")
+    plt.title(tit)
+    plt.xlabel('Episodes')
     plt.ylabel(label)
     output_dir = f"./Plots/Episode-{episode}"
     mkdir_p(output_dir)
     file_name=f'{tit}.png'
-    plt.savefig(f"{output_dir}/{file_name}")
-    plt.show()
+    plt.savefig(f"{output_dir}/{file_name}",bbox_inches='tight')
+    plt.show()    
+def utility(throughput_c, delay_c):
+    
+    r = 0
+    if throughput_c > 0:
+      r += math.log(throughput_c/bandwidth)
+    if delay_c > 0:
+      # r += -0.2 * math.log(delay_c);
+      r += -0.01 * math.log(delay_c)
+    
+    return r
 
-# agent.load_weights()
+def find_reward(u2, u1):
+    diff = u2 - u1
+    if diff >= 1:
+      return 10
+    elif 0<= diff < 1:
+      return 5
+    elif -1 <= diff < 0:
+      return -5
+    else:
+      return -10
+
+
+
+
 for e in range(iterationNum):
     current_state = env.reset()
+    node_id=current_state[3]
     current_state = current_state[4:]
     cWnd = current_state[1]
     init_cWnd = cWnd
     current_state = np.array([current_state])
+    running_tp=0
+    running_rew=0
+    running_rtt=0
+    cnt_tp=[0 for i in range(leafs)]
+    cnt_rew=[0 for i in range(leafs)]
+    cnt_rtt=[0 for i in range(leafs)]
+    avg_tp=[0 for i in range(leafs)]
+    avg_rew=[0 for i in range(leafs)]
+    avg_rtt=[0 for i in range(leafs)]
+    old_U=0
     for step in range(maxSteps):
 
         print("[+] Iteration:{} Step: {}".format(e+1,step+1), file=w_file)
         
         total_steps = total_steps + 1
-        # the agent computes the action to perform
-        action = agent.compute_action(current_state)
-        # change CWND
+        action = agent.compute_action(current_state,node_id)
         calc_cWnd = cWnd+action_mapping[action]
+        
     
         if calc_cWnd<0:
             calc_cWnd=cWnd
@@ -251,8 +329,12 @@ for e in range(iterationNum):
         # the next state, a reward and whether the agent is done
         actions = [new_ssThresh, calc_cWnd]
         next_state, reward, done, _ = env.step(actions)
-       
+        
+        new_U=utility(next_state[15],(next_state[11]-next_state[12]))
+        reward=find_reward(new_U,old_U)
+        old_U=new_U
         rewardsum += reward
+        node_id=next_state[3]
         #print("Next state: ",next_state)
         next_state = next_state[4:]
         # print(next_state)
@@ -266,16 +348,24 @@ for e in range(iterationNum):
         print("\t[!] RTT :",rtt,file=w_file)
         print("\t[!] Throughput: ",throughput,file=w_file)
         print("\t[!]Epsilon: ",agent.find_exploration_proba(),file=w_file)
-        rew_history.append(rewardsum)
-        rtt_history.append(rtt*1e-6)
-        cWnd_history.append(cWnd)
+        cWnd_history[node_id-2].append(cWnd)
+        rew_history[node_id-2].append(rewardsum)
+        rtt_history[node_id-2].append(rtt*1e-6)
+        
         if throughput==0:
             tp.append((step,e))
-        tp_history.append(throughput*8)
-        global_tp_history.append((throughput*8))
+        
+        tp_history[node_id-2].append(throughput*8)
+        
+        running_tp=running_tp+(throughput*8)
+        running_rew=running_rew+rewardsum
+        running_rtt=running_rtt+(rtt*1e-6)
+
+        global_tp_history.append(throughput*8)
         global_rew_history.append(rewardsum)
         global_rtt_history.append(rtt*1e-6)
-        global_cWnd_history.append(cWnd)
+        
+        
         
         next_state = np.array([next_state])
         # We store each experience in the memory buffer
@@ -288,38 +378,47 @@ for e in range(iterationNum):
                         file=w_file)
             break
         if total_steps >= agent.batch_size:
-           agent.train()
+           agent.train(node_id)
+           total_steps=0
+
         #update epsilon value
         agent.update_exploration_probability()  
         # agent.save_weights()
-        agent.save_model()
+        
+        # agent.update_exploration_probability() 
         current_state = next_state
     
-       
-    
+    # if total_steps >= agent.batch_size:
+    #        agent.train()
+      
+    agent.save_model() 
     print("\n[O] Iteration over.", file=w_file)
     print("[-] Final reward sum: ", rewardsum, file=w_file)
     print()
+    
     mpl.rcdefaults()
     mpl.rcParams.update({'font.size': 16})
-    plot_graphs(cWnd_history,'Congestion Windows','CWND (segments)',e)
-    plot_graphs(tp_history,'Throughput over time','Throughput (Mbits/s)',e)
-    plot_graphs(rtt_history,'RTT over time','RTT (seconds)',e)
-    plot_graphs(rew_history,'Reward sum plot','Accumulated reward',e)
-    cWnd_history=[]
-    tp_history=[]
-    rtt_history=[]
-    rew_history=[]
-    
-    # if the have at least batch_size experiences in the memory buffer
-    # than we tain our model
+    plot_individual_graphs(cWnd_history,'Congestion Windows','CWND',e)
+    plot_individual_graphs(tp_history,'Throughput over time','Throughput (Bits/s)',e)
+    plot_individual_graphs(rtt_history,'RTT over time','RTT (seconds)',e)
+    plot_individual_graphs(rew_history,'Reward sum plot','Accumulated reward',e)
+    plot_graphs(global_tp_history,'Avg Throughput over time','Throughput (Bits/s)',e)
+    plot_graphs(global_rtt_history,'Avg RTT over time','RTT (seconds)',e)
+    plot_graphs(global_rew_history,'Avg Reward sum plot','Accumulated reward',e)
+    rew_history = [[] for i in range(leafs)]
+    cWnd_history = [[] for i in range(leafs)]
+    rtt_history = [[] for i in range(leafs)]
+    tp_history = [[] for i in range(leafs)]
+    global_tp_history=[]
+    global_rtt_history=[]
+    global_rew_history=[]
+   
+  
     
 
-
-print(tp)       
-mpl.rcdefaults()
-mpl.rcParams.update({'font.size': 16})
-plot_graphs(global_cWnd_history,'Congestion Windows','CWND (segments)',iterationNum)
-plot_graphs(global_tp_history,'Throughput over time','Throughput (Mbits/s)',iterationNum)
-plot_graphs(global_rtt_history,'RTT over time','RTT (seconds)',iterationNum)
-plot_graphs(global_rew_history,'Reward sum plot','Accumulated reward',iterationNum)
+# plot_graphs(global_tp_history,'Avg Throughput over time','Throughput (Mbits/s)',iterationNum)
+# plot_graphs(global_rtt_history,'Avg RTT over time','RTT (seconds)',iterationNum)
+# plot_graphs(global_rew_history,'Avg Reward sum plot','Accumulated reward',iterationNum)
+# print(tp)       
+# mpl.rcdefaults()
+# mpl.rcParams.update({'font.size': 16})
